baf016ff27bff469f4c82690386d9fd445bd1c37
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-10-27 22:57:04 +0900

    Add: Managerモデル追加及びOffice紐付け
    Managerがコントローラー経由で作成される際、Officeも一緒に作成されます。
    この時、Managerの郵便番号からOfficeの経度、緯度及び最寄り駅を求めようとしますが、
    郵便番号の値によっては失敗することも十分にありえます（例えば、郵便番号が会社のものなど）。
    そのため、仮に経度、緯度及び最寄り駅が求められなくても、初期値（経度及び緯度はnil,最寄り駅は'未編集'）で登録し、
    フロントで手入力で登録できるようにしなければなりません（後回しで良いと思いますが）。

---
+++ app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ApplicationController < ApplicationController
+      private
+
+      # 未ログインまたはクライアントではない場合、エラーレンダリングする
+      def authenticate_api_v1_client!
+        return if authenticate_api_v1_user!
+
+        render_user_type_mismatch unless current_api_v1_user.client?
+      end
+
+      # 未ログインまたはケアマネではない場合、エラーレンダリングする
+      def authenticate_api_v1_manager!
+        return if authenticate_api_v1_user!
+
+        render_user_type_mismatch unless current_api_v1_user.manager?
+      end
+
+      def render_user_type_mismatch
+        json = {
+          status: 'error',
+          errors: {
+            full_messages: [
+              '実行に必要なアクセス権がありませんでした。'
+            ]
+          }
+        }
+
+        render json:, status: :unprocessable_entity
+      end
+    end
+  end
+end
