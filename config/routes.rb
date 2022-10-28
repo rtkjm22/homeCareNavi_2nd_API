fb1bbef30e05a4bb2863acdf3e985299f1b78a97
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-10-28 19:04:00 +0900

    Fix: ログイン時にユーザータイプを渡すよう機能変更
    以前まではクライアントページ、ケアマネページどちらからのログインでも、メールアドレスとパスワードがあっていればログイン出来てしまっていた。
    それを改善するため、ログイン時に新たに:typeパラメータを受け付けるようにし、メアドとパスワードがあっていても:typeが異なれば弾くようにした。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/overrides/registrations'
+        registrations: 'api/v1/overrides/registrations',
+        sessions: 'api/v1/overrides/sessions'
       }
     end
   end
