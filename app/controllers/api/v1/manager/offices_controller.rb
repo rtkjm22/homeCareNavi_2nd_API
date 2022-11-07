76153d518b671c0013e04d836d34afd29248e2e0
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-11-07 21:53:24 +0900

    Add: Api::V1::Manager::OfficesController#update追加

--- app/controllers/api/v1/manager/offices_controller.rb
+++ app/controllers/api/v1/manager/offices_controller.rb
@@ -18,6 +18,21 @@ module Api
           res = attrs.merge(thumbnail_image:, feature_images:, carousel_images:)
           render json: res, status: :ok
         end
+
+        def update
+          office = current_api_v1_manager.office
+          if office.update(office_params)
+            render json: office, status: :ok
+          else
+            render json: office.as_error_json, status: :bad_request
+          end
+        end
+
+        private
+
+        def office_params
+          params.permit(:name, :feature_title, :feature_detail, :workday_detail, :fax, workday: [])
+        end
       end
     end
   end
