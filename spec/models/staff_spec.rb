38e19b2aed1f21d8103d8686a71632062c975f48
akachiryo <rirugameri@icloud.com>
2022-11-16 22:52:41 +0900

    Delete:staff_spec.rb モデルの正常系テストを削除

--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -6,13 +6,6 @@ RSpec.describe Staff, type: :model do
       it { is_expected.to belong_to(:office) }
     end
 
-    context 'office_id、name、furiganaがある場合、有効である' do
-      it 'スタッフの登録ができる' do
-        staff = create(:staff)
-        expect(staff).to be_valid
-      end 
-    end
-
     context 'name' do
       it { is_expected.to validate_presence_of(:name) }
     end
