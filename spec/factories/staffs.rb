489591c6c384204d0508d99dac60be3a5cfdd4f8
akachiryo <rirugameri@icloud.com>
2022-11-16 22:56:32 +0900

    Fix:factories/staffs_rb roleを変更

--- spec/factories/staffs.rb
+++ spec/factories/staffs.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     name { Gimei.name.kanji }
     furigana { Gimei.name.hiragana }
     introduction { Faker::Lorem.sentence(word_count: 25) }
-    role { 1 }
+    role { 'care_manager' }
   end
 end
