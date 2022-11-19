1257472cb2dbd21f3d0a12c80969dbf3e05f6b11
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-11-19 18:30:01 +0900

    Add Officeモデルにエリア検索ロジック及びテストを追加

--- app/models/office.rb
+++ app/models/office.rb
@@ -16,4 +16,18 @@ class Office < ApplicationRecord
   validates :nearest_station, presence: true
 
   flag :workday, %i[sun mon tue wed thu fri sat]
+
+  # アドレスによる前方一致検索を実行する
+  # @param [String] area 都道府県及び市区町村
+  # @example
+  #   Office.search_by_area("東京都新宿区市谷")
+  scope :search_by_area, lambda { |area|
+    eager_load(:manager, :office_images)
+      .where(
+        'users.address LIKE ?',
+        # 参考: https://railsguides.jp/active_record_querying.html#%E6%9D%A1%E4%BB%B6%E3%81%A7like%E3%82%92%E4%BD%BF%E3%81%86
+        "#{sanitize_sql_like(area)}%"
+      )
+      .order(:created_at)
+  }
 end
