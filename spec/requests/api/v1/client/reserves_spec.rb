b05216de5a00f8abb75df32644b9332ae48ab766
akachiryo <rirugameri@icloud.com>
2022-12-10 00:39:24 +0900

    Add:POST /api/v1/client/reservesのリクエストテスト作成

--- spec/requests/api/v1/client/reserves_spec.rb
+++ spec/requests/api/v1/client/reserves_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Client::Reserves", type: :request do
+
   describe "GET /api/v1/client/reserves" do
     let(:client) { create(:client) }
     let(:manager) { create(:manager) }
@@ -25,6 +26,37 @@ RSpec.describe "Api::V1::Client::Reserves", type: :request do
       login manager
 
       get api_v1_client_reserves_path
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe 'POST /api/v1/client/reserves' do
+    let(:client) { create(:client) }
+    let(:manager) { create(:manager) }
+
+    it '自分の事業所の、スタッフを作成できること' do
+      reserve = create(:reserve)
+      login client
+      reserve_params = { office_id: reserve.office_id,
+                         interview_begin_at: reserve.interview_begin_at,
+                         interview_end_at: reserve.interview_end_at,
+                         user_name: reserve.user_name,
+                         user_age: reserve.user_age,
+                         contact_tel: reserve.contact_tel,
+                         note: reserve.note
+                       }
+      expect { post api_v1_client_reserves_path, params: reserve_params }.to change(client.reserves, :count).by(1)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'ログインしていない場合、エラーが返ってくること' do
+      post api_v1_client_reserves_path
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it 'マネ―ジャーでログインした場合、エラーが返ってくること' do
+      login manager
+      post api_v1_client_reserves_path
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
