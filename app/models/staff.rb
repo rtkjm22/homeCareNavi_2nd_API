8f4344f1d9bb8cfe5f464e98356a733569d62e91
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-12-18 14:10:07 +0900

    Add: Staffに画像を添付できるよう機能追加

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,7 +2,14 @@ class Staff < ApplicationRecord
   enum role: { worker: 0, care_manager: 1 }, _prefix: true
   belongs_to :office
   has_many :office_clients, dependent: :restrict_with_error
+  has_one_attached :avatar
 
   validates :name, presence: true
   validates :furigana, presence: true
+  validates :avatar, content_type: { in: %w[image/jpg image/jpeg image/png] },
+                     size: { less_than: 5.megabytes }
+
+  def avatar_url
+    avatar.attached? ? Rails.application.routes.url_helpers.url_for(avatar) : nil
+  end
 end
