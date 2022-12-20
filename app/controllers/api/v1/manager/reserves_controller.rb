a7f73ae6333686d68e798408a71f11513f0be19d
akachiryo <rirugameri@icloud.com>
2022-12-20 23:14:31 +0900

    Add:予約更新(ケアマネ)　機能実装

--- app/controllers/api/v1/manager/reserves_controller.rb
+++ app/controllers/api/v1/manager/reserves_controller.rb
@@ -8,6 +8,18 @@ module Api
           reserves = current_api_v1_manager.office.reserves
           render json: reserves, status: :ok
         end
+
+        def update
+          reserves = current_api_v1_manager.office.reserves
+          reserve_to_update = reserves.find(params[:id])
+
+          if reserve_to_update.update!(is_contacted: true)
+            render json: reserve_to_update, status: :ok
+          else
+            render json: reserve_to_update.as_error_json, status: :bad_request
+          end
+        end
+
       end
     end
   end
